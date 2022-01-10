d1f78a8a5b1ebd96beba7f1089d50239f7b13ab5
Hiroshi Isobe <isobe69b@gmail.com>
2022-01-10 15:19:17 -0800

    Connect to Firebase and configure fetch request

--- react-course/src/pages/NewMeetups.js
+++ react-course/src/pages/NewMeetups.js
@@ -2,7 +2,14 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupsPage() {
   function addMeetupHandler(meetupData) {
-    
+    fetch("https://react-getting-started-beea1-default-rtdb.firebaseio.com/meetups.json",
+    {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   }
   return <section>
     <h1>Add New Meetup</h1>
