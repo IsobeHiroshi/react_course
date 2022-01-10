600fb33df3bfa4745938074bc24710d0d45c68d8
Hiroshi Isobe <isobe69b@gmail.com>
2022-01-10 09:52:37 -0800

    Change router statement to v6 formet

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AllMeetupsPage from "./pages/NewMeetups";
+import NewMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<AllMeetupsPage />} />
+      <Route path="/new-meetup" element={<NewMeetupsPage />} />
+      <Route path="/favorites" element={<FavoritesPage />}/>
+    </Routes>
   );
 }
 
