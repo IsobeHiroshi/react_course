5faa0c1384c505fea76072ebec6c562699837aa2
Hiroshi Isobe <isobe69b@gmail.com>
2022-01-15 20:24:21 -0800

    Enable dinamic rendering of the button text in All mettups page

--- react-course/src/components/meetups/MeeupItem.js
+++ react-course/src/components/meetups/MeeupItem.js
@@ -35,7 +35,7 @@ function MeetupItem(props) {
           <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-          <button onClick={toggleFavoritesStatusHandler}>To Favorites</button>
+          <button onClick={toggleFavoritesStatusHandler}>{itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}</button>
         </div>
       </Card>
     </li>
